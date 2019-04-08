35d584ead92d1d8373ff016bc1e48732eb9d933a
Pawel Kopec <paweelkopec@gmail.com>
2019-04-08 22:51:01 +0200

    Sample REST service

---
+++ src/main/java/com/pawelk/javadev/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.pawelk.javadev.controllers;
+
+import com.pawelk.javadev.models.Home;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/home")
+    public Home home(@RequestParam(value="user", defaultValue = "Guest") String user){
+        return new Home("Hello "+user);
+    }
+}
---
+++ src/main/java/com/pawelk/javadev/models/Home.java
@@ -0,0 +1,16 @@
+package com.pawelk.javadev.models;
+
+public class Home {
+
+    private final String content;
+
+    public Home(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
+
