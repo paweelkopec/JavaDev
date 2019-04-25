7f514a1c3af7abe17f69f401bd9be720bea975db
Pawel Kopec <paweelkopec@gmail.com>
2019-04-25 22:58:17 +0200

    Postgres support

--- src/main/java/com/pawelk/javadev/controllers/HomeController.java
+++ src/main/java/com/pawelk/javadev/controllers/HomeController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    @RequestMapping("/")
+    public String index(){
+        return "Welcome home ";
+    }
+
     @RequestMapping("/home")
     public Home home(@RequestParam(value="user", defaultValue = "Guest") String user){
         return new Home("Hello "+user);
