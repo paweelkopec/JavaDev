74a94129ef2805905c492c4a439e04e40a5272dd
Pawel Kopec <paweelkopec@gmail.com>
2019-05-29 17:43:26 +0200

    Fronted App

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {AppService} from "./app.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
+  logout() {
+    this.app.authenticated = false;
+    this.router.navigateByUrl('/login');
+    // this.http.post('logout', {}). finally(() => {
+    //   this.app.authenticated = false;
+    //   this.router.navigateByUrl('/login');
+    // }).subscribe();
+  }
+
+}
+
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {Injectable, NgModule} from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import {AppService} from "./app.service";
+import {FormsModule} from "@angular/forms";
+
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
